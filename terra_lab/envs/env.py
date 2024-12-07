14d66285aafc6b901a0680cdd3892bee1eaf6739
MarcMalha <marc.malha@beanstock.com>
2024-12-07 17:58:26 +0100

    Feat: randomize rock positions when the map is initialized

--- terra_lab/envs/env.py
+++ terra_lab/envs/env.py
@@ -14,9 +14,51 @@ class Env:
         self.state = np.zeros((self.grid_size, self.grid_size), dtype=np.int32)
         self.initialize_map()
 
+    @staticmethod
+    def generate_random_coordinates(lower_boundary, upper_boundary):
+        return (
+                np.int32(np.ceil(np.random.rand() * upper_boundary + lower_boundary)),
+                np.int32(np.ceil(np.random.rand() * upper_boundary + lower_boundary)),
+            )
+
+    @staticmethod
+    def distance(coordinate1, coordinate2):
+        return np.abs(coordinate1 - coordinate2)
+
+    @staticmethod
+    def find_neighbors_with_invalid_distance(existing_rocks_coordinates, new_rock_coordinates, invalid_distance):
+        """Returns a list of neighboring rocks that whose distance with the new_rock is less than or equal invalid_distance"""
+        new_rock_x, new_rock_y = new_rock_coordinates
+        neighbors = list(filter(lambda existing_rock_coordinates: Env.distance(new_rock_x, existing_rock_coordinates[0]) <= invalid_distance \
+                                                                  or Env.distance(new_rock_y, existing_rock_coordinates[1]) <= invalid_distance
+                        , existing_rocks_coordinates))
+
+        return neighbors
+
+    def randomize_initial_rock_positions(self, count_rocks = 4):
+        wind_turbine_limits = (MACHINE_TYPE.WIND_TURBINE.value.range - 1, self.grid_size - MACHINE_TYPE.WIND_TURBINE.value.range)
+
+        lower_boundary, upper_boundary = wind_turbine_limits
+        upper_boundary -= lower_boundary
+        initial_rock_positions = [Env.generate_random_coordinates(lower_boundary, upper_boundary)]
+        for _ in range(count_rocks - 1):
+            random_coordinates_within_limits = Env.generate_random_coordinates(lower_boundary, upper_boundary)
+
+            # Checking that the new rock is not near any already generated rock
+            invalid_neighbors = Env.find_neighbors_with_invalid_distance(initial_rock_positions, random_coordinates_within_limits, MACHINE_TYPE.WIND_TURBINE.value.range - 1)
+            # if new rock is too close to an existing rock, generate new random coordinates until we get a valid result
+            while len(invalid_neighbors) > 0:
+                random_coordinates_within_limits = Env.generate_random_coordinates(lower_boundary, upper_boundary)
+                invalid_neighbors = Env.find_neighbors_with_invalid_distance(initial_rock_positions, random_coordinates_within_limits, MACHINE_TYPE.WIND_TURBINE.value.range - 1)
+
+            initial_rock_positions.append(random_coordinates_within_limits)
+
+        return initial_rock_positions
+
     def initialize_map(self):
         """Initializes the map with some predefined state."""
-        initial_positions = [(2, 2), (8, 2), (2, 8), (10, 10)]
+        initial_positions = self.randomize_initial_rock_positions()
+
         for x, y in initial_positions:
             self.state[x, y] = 1
 
